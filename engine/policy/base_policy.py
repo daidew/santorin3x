b240f8c9728caba3362da7ceecbd4d0904d299d9
Anan Methasate <anan_m@me.com>
2019-11-14 09:02:31 +0700

    add human player

--- engine/policy/base_policy.py
+++ engine/policy/base_policy.py
@@ -28,3 +28,29 @@ class RandomPolicy():
 
     def optimize_policy(self, data):
         pass
+
+class HumanPlayer():
+    '''
+    Interactive human player which able receive input and act accordingly.
+    '''
+    def __init__(self):
+        pass
+
+    def step(self, env):
+        all_legal_moves = env.legal_moves()
+        if len(all_legal_moves) == 0: #no legal moves available
+            return -1
+        
+        #convert all legal actions to action index
+        all_legal_actions = []
+        for legal_move in all_legal_moves:
+            all_legal_actions.append(legal_move)
+
+        while True:
+            #(ex. -1 a q)
+            w, m, b = input('Please enter next move: ')
+            action_idx = env.atoi[(int(w), m, b)]
+            if action_idx not in all_legal_moves:
+                print('You entered illegal moves. Please try again.')
+            else:
+                return action_idx
